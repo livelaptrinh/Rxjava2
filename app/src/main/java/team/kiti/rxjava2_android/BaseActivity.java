d81d6d4720b1c48f9333980b508fc62c0dddb9eb
livelaptrinh <livelaptrinh@gmail.com>
2018-01-31 10:10:10 +0700

    a

---
+++ app/src/main/java/team/kiti/rxjava2_android/BaseActivity.java
@@ -0,0 +1,11 @@
+package team.kiti.rxjava2_android;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Thien on 1/22/2018.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    protected String TAG = "OKE";//
+}
